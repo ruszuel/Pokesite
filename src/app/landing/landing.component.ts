d9fc47a4deb2d8b7716f6038a5634cf8fb8ef5c6
Russuel <147899891+ruszuel@users.noreply.github.com>
2025-03-12 10:47:20 +0800

    add carousel with flickity

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
-import { Component, computed, inject } from '@angular/core';
+import { AfterViewInit, Component, effect, ElementRef, inject, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { landingService } from './landing.service';
 import { Router } from '@angular/router';
+import Flickity from 'flickity';
 
 @Component({
   selector: 'app-landing',
@@ -8,10 +9,40 @@ import { Router } from '@angular/router';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
+  @ViewChild('carousel', { static: false }) carouselElement!: ElementRef
   private landingService = inject(landingService)
   sliders = this.landingService.allPokemon
   private route = inject(Router)
+
+  flickityInstance: any
+
+  ngAfterViewInit() {
+    this.flickityInstance = new Flickity(this.carouselElement.nativeElement, {
+      cellAlign: 'center',
+      contain: true,
+      wrapAround: true,
+      autoPlay: 3000,
+      pageDots: false,
+      percentPosition: false,
+      resize: true,
+      initialIndex: 1
+    });
+
+
+    setTimeout(() => {
+      this.flickityInstance.resize();
+      this.flickityInstance.reposition();
+
+      const cells = this.carouselElement.nativeElement.querySelectorAll('.carousel-cell:not(.is-selected)');
+      cells.forEach((cell: HTMLElement) => {
+        cell.style.alignSelf = 'center';
+      });
+    }, 100);
+  }
+
+
+
   onClick() {
     this.route.navigate(['pokemons'])
   }
